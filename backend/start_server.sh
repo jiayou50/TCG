9b797d0a8bf79197ba543df150720af476670420
You Jia <jiayou50@users.noreply.github.com>
2026-03-23 19:08:30 -0700

    Update start_server.sh

--- backend/start_server.sh
+++ backend/start_server.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Use local Python environment.
+source ./myenv/bin/activate
+
 # Start the backend FastAPI development server.
 uvicorn tcg_engine.api:app --app-dir src --reload
